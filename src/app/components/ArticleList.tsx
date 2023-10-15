8aa68bcc2b0510651cca6a82f9ccf9ea686920d6
nx2141 <super_red_block@yahoo.co.jp>
2023-10-16 01:53:59 +0900

    dev

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,76 +1,47 @@
+import { Article } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=8"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link className="text-blue-700 pb-4 font-bold" href="#">
-            Technology
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://source.unsplash.com/collection/1346951/1000x500?sig=8"
+              alt=""
+              width={1280}
+              height={300}
+            />
           </Link>
-          <Link
-            className="text-slate-900 text-3xl font-bold hover:text-gray-70 pb-4"
-            href="#"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">
-            published on 2023/07/15
-          </p>
-          <Link className="text-slate-900 pb-6" href="#">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cum
-            soluta ipsa reprehenderit aperiam odio pariatur fugiat, sapiente
-            minus laborum debitis perferendis alias accusamus qui reiciendis est
-            ut velit libero.
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link className="text-blue-700 pb-4 font-bold" href="#">
-            Technology
-          </Link>
-          <Link
-            className="text-slate-900 text-3xl font-bold hover:text-gray-70 pb-4"
-            href="#"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">
-            published on 2023/07/15
-          </p>
-          <Link className="text-slate-900 pb-6" href="#">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cum
-            soluta ipsa reprehenderit aperiam odio pariatur fugiat, sapiente
-            minus laborum debitis perferendis alias accusamus qui reiciendis est
-            ut velit libero.
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link className="text-blue-700 pb-4 font-bold" href="#">
+              Technology
+            </Link>
+            <Link
+              className="text-slate-900 text-3xl font-bold hover:text-gray-70 pb-4"
+              href={`articles/${article.id}`}
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">
+              published on {article.createdAt}
+            </p>
+            <Link className="text-slate-900 pb-6" href="#">
+            {article.content}
+            </Link>
+            <Link href="#" className="text-pink-800 hover:text-black">
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import { getAllArticles } from "@/blogAPI";
 import ArticleList from "./components/ArticleList";
 // import { getAllArticles } from "@/pages/api/blogAPI";
 
-export default function Home() {
+export default async function Home() {
+
+  const articles = await getAllArticles();
 
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles= {articles} />
         <div className="flex items-center py-8">
           <a
             href="#"
